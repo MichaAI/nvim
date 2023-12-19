e3f9b966e080e46c7298ad3709a091d706e94a26
MichaAI <misha-8-filatov@yandex.ru>
2023-12-19 23:02:36 +0300

    екл

--- lua/plugins/init.lua
+++ lua/plugins/init.lua
@@ -4,6 +4,9 @@ if fn.empty(fn.glob(install_path)) > 0 then
   packer_bootstrap = fn.system({'git', 'clone', '--depth', '1', 'https://github.com/wbthomason/packer.nvim', install_path})
 end
 
+require('lsp_config')
+
+
 return require('packer').startup(function(use)
   use 'wbthomason/packer.nvim'
 
@@ -14,6 +17,8 @@ return require('packer').startup(function(use)
   -- конфиги для LSP серверов, нужен для простой настройки и
   -- возможности добавления новых серверов
   use 'neovim/nvim-lspconfig'
+  use 'powerman/vim-plugin-ruscmd'
+  use 'dense-analysis/ale'
 
   -- зависимости для движка автодополнения
   use 'hrsh7th/cmp-nvim-lsp'
@@ -39,11 +44,37 @@ return require('packer').startup(function(use)
   use('onsails/lspkind-nvim')
 
   use {
-  'nvim-lualine/lualine.nvim',
-  requires = { 'nvim-tree/nvim-web-devicons', opt = true }
+    'nvim-lualine/lualine.nvim',
+    config = function()
+      require('lualine').setup {
+        options = { theme = 'onedark' },
+      }
+    end
+  }
+  
+  use {
+  'pwntester/octo.nvim',
+  requires = {
+    'nvim-lua/plenary.nvim',
+    'nvim-telescope/telescope.nvim',
+    'nvim-tree/nvim-web-devicons',
+  },
+  config = function ()
+    require"octo".setup()
+  end
+  }
+  
+  use {
+  "NeogitOrg/neogit",
+  dependencies = {
+    "nvim-lua/plenary.nvim",         -- required
+    "sindrets/diffview.nvim",        -- optional - Diff integration
+  },
+  require('neogit').setup()
   }
 
   if packer_bootstrap then
     require('packer').sync()
   end
 end)
+
